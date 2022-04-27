0940e048246d8a2ef4f03a1c903ee80b03107fe8
Amir Hossain <amirhossain.cse113@gmail.com>
2022-04-28 01:04:58 +0600

    first-commit

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/**
+ * ----------------------------
+ * One Time for my computer
+ * ----------------------------
+ * 1. Create heroku account
+ * 2. Verify Email
+ * 3. install heroku cli
+ * 4. heroku login
+ * -----------------------------
+ * for each project one time
+ * -----------------------------
+ * 1. heroku create
+ * 2. make sure you: git add . git commit git push
+ */
